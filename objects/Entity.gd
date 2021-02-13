bed2024e9cace8d12ad8b368cfc5175157df92d1
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-02-13 13:47:00 -0800

    Added base Entity class for player and enemies to inherit from.

---
+++ objects/Entity.gd
@@ -0,0 +1,20 @@
+extends Node
+class_name Entity
+
+
+
+export var max_health : int
+
+var health : int
+var attack_damage : int
+
+
+
+# Virtuals
+func die() -> void: pass
+# EOF: Virtuals
+
+
+
+func damage(amount : int) -> void:
+	pass
