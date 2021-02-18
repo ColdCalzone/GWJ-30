9dcf6eafa266ffd9c0a778912985f4b6e6deadf6
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-02-17 19:23:28 -0800

    Gave Enemy1 a CollisionShape2D, Tween, and a damage animation.

--- objects/enemies/Enemy1.gd
+++ objects/enemies/Enemy1.gd
@@ -5,6 +5,7 @@ extends Entity
 export var target_player_distance : int
 
 onready var attack_timer := $AttackTimer
+onready var tween := $Tween
 
 onready var target := get_random_player()
 
@@ -28,4 +29,14 @@ func get_near_player() -> void:
 func attempt_attack() -> void:
 	if attack_timer.time_left == 0:
 		attack_timer.start()
-		CombatManager.attack(self, SwingAttackParams.new(global_position, rad2deg(get_angle_to(target.global_position) + 90)))
+		var angle := rad2deg(get_angle_to(target.global_position))
+		var offset := Vector2(cos(deg2rad(angle)), sin(deg2rad(angle))) * 40
+		CombatManager.attack(self, SwingAttackParams.new(global_position + offset, angle + 90))
+
+
+
+func damaged() -> void:
+	tween.stop_all()
+	tween.interpolate_property(self, "rotation_degrees", [-27, 27][randi() % 2], 0, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	tween.interpolate_property(self, "modulate", Color.red, Color.white, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	tween.start()
