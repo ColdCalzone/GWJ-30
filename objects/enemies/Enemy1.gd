494da59bb925e35e92b27b48bc75edbe2ae93a8f
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-02-20 09:19:04 -0800

    Enemy1 now sets max_health and health

--- objects/enemies/Enemy1.gd
+++ objects/enemies/Enemy1.gd
@@ -6,8 +6,13 @@ export var target_player_distance : int
 
 onready var attack_timer := $AttackTimer
 onready var tween := $Tween
-
 onready var target := get_random_player()
+
+
+
+func _ready() -> void:
+	max_health = 10
+	health = 10
 
 
 
@@ -40,10 +45,10 @@ func damaged() -> void:
 	tween.interpolate_property(self, "rotation_degrees", [-27, 27][randi() % 2], 0, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	tween.interpolate_property(self, "modulate", Color.red, Color.white, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	tween.start()
-	print(health)
 
 
 
 func die() -> void:
 	ParticleManager.spawn(PulseParticleParams.new(global_position))
+	game.add_kill()
 	queue_free()
