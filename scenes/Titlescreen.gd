a18d10fe2a812074e85647298df53900dc62d140
ColdCalzone <coldhotpocket1@gmail.com>
2021-02-21 08:56:59 -0500

    Main Menu.
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA?

---
+++ scenes/Titlescreen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+onready var play = $MenuCenter/MenuVBox/Play
+onready var exit = $MenuCenter/MenuVBox/Exit
+
+onready var game = load("res://scenes/Game.tscn")
+onready var exit_workaround = load("res://scenes/Exit.tscn")
+
+func _ready():
+	play.connect("pressed", self, "button_pressed", ["play"])
+	exit.connect("pressed", self, "button_pressed", ["exit"])
+
+func button_pressed(id : String):
+	match id:
+		"play": TransitionManager.transition(game)
+		"exit": TransitionManager.transition(exit_workaround)
