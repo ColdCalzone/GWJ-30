f09413e7e2bfc2b11c65dc29dc2a8928fc3e35a4
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-02-21 10:37:25 -0800

    Gameover screen now displays stats

--- scenes/Gameover.gd
+++ scenes/Gameover.gd
@@ -12,7 +12,7 @@ const TITLE_SCREEN := preload("res://scenes/Titlescreen.tscn")
 func _ready() -> void:
 	Map.tile_map.clear()
 	waves_label.text = "Waves Survived: %s" % [GlobalData.waves_survived]
-	kills_label.text = "Enemies killed: %" % [GlobalData.enemies_killed]
+	kills_label.text = "Enemies killed: %s" % [GlobalData.enemies_killed]
 
 
 
