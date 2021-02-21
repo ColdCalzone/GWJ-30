0d0c316ab5cfa285cb42ceb420b9044e70a2da27
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-02-21 05:52:45 -0800

    Added fallback to make sure body still exists because idk why

--- objects/Superpower.gd
+++ objects/Superpower.gd
@@ -9,6 +9,7 @@ export var amount : int
 
 
 func _on_Area2D_body_entered(body : PhysicsBody2D) -> void:
-	if body.is_in_group("player"):
-		body.set(modifying_variable, body.get(modifying_variable) + amount)
-		queue_free()
+	if body != null:
+		if body.is_in_group("player"):
+			body.set(modifying_variable, body.get(modifying_variable) + amount)
+			queue_free()
