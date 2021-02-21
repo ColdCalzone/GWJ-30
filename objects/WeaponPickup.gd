e847e0a43319095cb8d42922cd06c1daba467b4d
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-02-20 21:39:41 -0800

    Added WeaponPickup

---
+++ objects/WeaponPickup.gd
@@ -0,0 +1,34 @@
+extends Node2D
+class_name WeaponPickup
+
+
+
+onready var sprite := $Sprite
+onready var cooldown := $CooldownTimer
+
+var weapon_name : String
+
+
+
+func _ready() -> void:
+	update_texture()
+
+
+
+func update_texture() -> void:
+	sprite.set_texture(load(WeaponManager.get_weapon_data(weapon_name).texture))
+
+
+
+func _on_Area2D_body_entered(body : PhysicsBody2D) -> void:
+	if body is Entity && !body.dead && !cooldown.time_left:
+		body.drop_chance = 1.00
+		print(weapon_name)
+		body.set_weapon(weapon_name)
+		queue_free()
+
+
+
+func _on_LifeTimer_timeout() -> void:
+	ParticleManager.spawn(PulseParticleParams.new(global_position))
+	queue_free()
