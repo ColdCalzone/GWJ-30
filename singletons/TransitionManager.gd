020ff0cd77dc572afc7e25ab3227ed463252f812
IAmNotIsaac <isaacjulianastell@gmail.com>
2021-02-21 03:22:22 -0800

    Updated TransitionManager

--- singletons/TransitionManager.gd
+++ singletons/TransitionManager.gd
@@ -8,5 +8,6 @@ onready var animation_player := $AnimationPlayer
 
 func transition(desired_scene : PackedScene) -> void:
 	animation_player.play("fade_in")
+	yield(animation_player, "animation_finished")
 	get_tree().change_scene_to(desired_scene)
 	animation_player.play("fade_out")
